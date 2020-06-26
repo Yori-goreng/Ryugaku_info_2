b2a417d58c6cdcffd412ae958bb5c60470438edb
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-06-26 20:04:28 +0900

    navbarから地図画面に飛べるよう実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'comments#index'
   get 'comments/index'
 
-  resources :maps, only: [:index]
+  resources :maps, only: [:index, :show]
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
