6b51f846d4554cc7270b43d027d87664010a5d73
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-06-26 13:07:25 +0900

    ゲストログイン機能を実装し、commnets/index.html.erbにゲストログインボタンを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'comments#index'
   get 'comments/index'
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end
