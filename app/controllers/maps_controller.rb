999af3cc3fd42ec641c825d13e96b79c932c66ce
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-06-26 22:36:13 +0900

    map検索実装

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -6,10 +6,13 @@ class MapsController < ApplicationController
   end
 
   def map
+  #map.js.erbで、経度緯度情報が入った@latlngを使えるように
+  results = Geocoder.search(params[:address])
+  @latlng = results.first.coordinates
+
   # respond_to以下の記述によってremote: trueのアクセスに対して、map.js.erbが使えるように
-    respond_to do |format|
+  respond_to do |format|
     format.js
-    end
   end
-  
+  end
 end
