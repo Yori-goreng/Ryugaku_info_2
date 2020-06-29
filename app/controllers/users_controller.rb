17e642ffae889a31a241f3f800f0704a71af0ed8
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-06-29 14:40:07 +0900

    users/edit追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  def index
-    @user = User.order(id: :asc)
+  def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -8,18 +8,26 @@ class UsersController < ApplicationController
   end
 
   def create 
-    user = User.create!(user_params)
-    redirect_to user
+    if @user.save
+      log_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @user = User.order(id: :asc)
   end
 
   def destroy
     user = User.find(parmas[:id])
     user.destroy!
     redirect_to user
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
 
   private
