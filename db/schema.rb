385f3bbf09b73f52b784c0cd39c1dfbc4481df9f
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-07-08 12:18:57 +0900

    paginateの日本語化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_080617) do
+ActiveRecord::Schema.define(version: 2020_07_08_001508) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create(:title => 'フィリピン', :schoolname => 'フィリピン学校1')
+Book.create(:title => 'フィリピン', :schoolname => 'フィリピン学校2')
+Book.create(:title => 'フィリピン', :schoolname => 'フィリピン学校3')
+
+5.times do |no|
+  Book.create(:schoolname => 'タイトル #{no}')
