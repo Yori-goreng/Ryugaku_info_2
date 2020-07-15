e8d0e658454938f91001785a9e42f499e2b6ca51
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-07-15 12:57:06 +0900

    noteをmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_080617) do
+ActiveRecord::Schema.define(version: 2020_07_15_035619) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
