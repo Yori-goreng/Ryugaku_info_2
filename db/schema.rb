df2f228c4e95213549499e019f72c2f1b0c282ea
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-07-15 13:34:57 +0900

    いいねランキングを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_035731) do
+ActiveRecord::Schema.define(version: 2020_07_15_042727) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_07_15_035731) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "note_id"
   end
 
   create_table "notes", force: :cascade do |t|
